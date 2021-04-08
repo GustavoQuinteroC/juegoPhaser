fa7fc96e1a2d3ad9dcb4402614405a5313b2cb46
GustavoQuinteroC <80115794+GustavoQuinteroC@users.noreply.github.com>
2021-04-08 13:32:00 -0500

    Add files via upload

---
+++ src/bootloader.js
@@ -0,0 +1,23 @@
+class Bootloader extends Phaser.Scene{
+    constructor(){
+        super({key: "Bootloader"});
+    }
+    preload(){
+        this.load.on
+        ("complete",()=>
+            {
+                this.scene.start("Scene_play");
+            }
+        );
+        this .scene.start("Scene_play");
+        this.load.image("ball","./assets/ball.png");
+        this.load.image("left","./assets/left_pallete.png");
+        this.load.image("right","./assets/right_pallete.png");
+        this.load.image("separator","./assets/separator.png");
+        this.load.image("fondo","../assets/fondo.png");
+    }
+
+    
+}
+
+export default Bootloader;
